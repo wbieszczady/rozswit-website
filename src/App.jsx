d5b44a91d0172c5e40d43a42c6736d296b723f57
Oskar <tobolewski.oskar@gmail.com>
2025-07-19 19:36:48 +0200

    Migrating to Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Witam serdecznie</h1>
+      <p>To jest taki o tescik</p>
+    </div>
+  )
+}
+
+export default App
